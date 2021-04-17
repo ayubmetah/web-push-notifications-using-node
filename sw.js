f39cd2508e757235f27e1d96950798124e568478
Ayub Metah <admin@ayubtechnologies.co.ke>
2021-04-17 22:58:43 +0300

    initial

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 self.addEventListener('push', function(event) {
     console.log('Received a push message', event);
-    self.registration.sendNotification('test message', {});
+    self.ServiceWorkerRegistration.sendNotification('test message', {});
 });
