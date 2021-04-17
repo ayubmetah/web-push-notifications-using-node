bc2ac456ed748539010a0c39a4df61b9c850d30f
Ayub Metah <admin@ayubtechnologies.co.ke>
2021-04-17 23:38:37 +0300

    fix

--- push.js
+++ push.js
@@ -5,15 +5,8 @@ let vapidKeys = {
     privateKey: 'emDznNmYQ5nAWqF8tNzQW7bZIEYVWkItWJALuhJeAKQ'
 }
 
-push.setVapidDetails('mailto: metta.job@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey)
+push.setVapidDetails('mailto: metta.job@gmail.com', vapidKeys.publicKey, vapidKeys.privateKey);
 
-let sub = {
-    "endpoint": "https://fcm.googleapis.com/fcm/send/fhjF05W5JLs:APA91bGJ4MR7_pcmBM81BvIkCxgjAbcZPunGW4lGzTBVjDJ09aIGQw4mY4W6pjbD8CiY5J02V88wopBkxowcV0uYWmHQMSTA-XEwCHdNuamA6GvUOphtAaMCLv1x6GakA2_I5ziWJ5pA",
-    "expirationTime": null,
-    "keys": {
-        "p256dh": "BErmuS66PRW_ZLAKmq03yBshxHV5shcfHgcFCjBcil8qYOoYXS7jmmGcOVzAonTkwLloGRHjvo3NMchSjPVrZrk",
-        "auth": "wkPHgfnIYYZSqQFXSta60g"
-    }
-}
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/fhjF05W5JLs:APA91bGJ4MR7_pcmBM81BvIkCxgjAbcZPunGW4lGzTBVjDJ09aIGQw4mY4W6pjbD8CiY5J02V88wopBkxowcV0uYWmHQMSTA-XEwCHdNuamA6GvUOphtAaMCLv1x6GakA2_I5ziWJ5pA", "expirationTime": null, "keys": { "p256dh": "BErmuS66PRW_ZLAKmq03yBshxHV5shcfHgcFCjBcil8qYOoYXS7jmmGcOVzAonTkwLloGRHjvo3NMchSjPVrZrk", "auth": "wkPHgfnIYYZSqQFXSta60g" } }
 
-push.sendNotification(sub, 'test message')
+push.sendNotification(sub, 'test message');
